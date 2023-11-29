b617517cb33d3e6f7a4d90653cc3a12c5ccee5ab
alexramirez02 <alexanderd.ramirez@udea.edu.co>
2023-11-29 08:07:03 -0500

    Revision

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 
 const Home =()=> {
   return (
-    <div>Home</div>
+    <div className='flex flex-col items-center'>
+      <div className='w-20 h-2 bg-#fde047 debug flex flex-col items-center ustify-center'></div>
+    </div>
   );
 };
 export default Home;
+
